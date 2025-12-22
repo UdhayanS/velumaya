1d82ef5e7ec4d9ab6897d1b2289a986656266c7f
Udhayan <udhayan.sennakesavan@rapiddatatech.com>
2025-12-22 18:19:08 +0530

    Updated complete project layout and styles

---
+++ js/custom.js
@@ -0,0 +1,72 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+$(window).load(function(){
+    $('.preloader').fadeOut(1000); // set duration in brackets    
+});
+
+
+/* Mobile Navigation
+    -----------------------------------------------*/
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+ /* Parallax section - disabled for smooth scrolling performance
+    -----------------------------------------------*/
+  // initParallax removed - CSS handles backgrounds without scroll lag
+
+
+  /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-speakers").owlCarousel({
+      autoPlay: 6000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  });
+
+
+  /* Back top
+  -----------------------------------------------*/
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
